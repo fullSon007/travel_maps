4045301deea0e47d515e5f27c689f0cab10fe2ed
Đào Thái Sơn <thaisonn0701@gmail.com>
2023-11-15 23:26:49 +0700

    initial travel map proj

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+// import GoogleMapReact from "google-map-react";
+// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import {LocationOutlinedIcon } from "@material-ui/icons/LocalActivityOutlined";
+// import { Rating } from "@material-ui/lab";
+
+// import makeStyles from "./style";
+
+const Map = () => {
+    // const classes = makeStyles();
+    // const isMoblie = useMediaQuery('(min-with:600px)');
+
+    // const coodinates = { lat: 0, lng: 0};
+    return (
+        // <div className={classes.mapContainer}>
+        //     <GoogleMapReact 
+        //         bootstrapURLKeys={{key: ''}}
+        //         defaultCenter={coodinates}
+        //         center={coodinates}
+        //         defaultZoom={14}
+        //         margin={[50,50,50,50]}
+        //         options={}
+        //         onChange={}
+        //     >
+
+        //     </GoogleMapReact>
+
+        // </div>
+
+        <h1>Map</h1>
+    );
+}
+
+export default Map;
