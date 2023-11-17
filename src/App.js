3c133d27bac15206cccf8217fbde14967692b26d
Đào Thái Sơn <thaisonn0701@gmail.com>
2023-11-17 23:23:37 +0700

    Doing maps part and working with Apis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-
+import { getPlacesData } from "./api";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(()=>{
+        getPlacesData()
+            .then((data) => {
+                console.log(data);
+
+                setPlaces(data);
+            })
+    }, []);
+
     return(
         <>
             <CssBaseline />
@@ -18,7 +32,11 @@ const App = () => {
                 </Grid>
 
                 <Grid item xs={12} md={8} >
-                    <Map />
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,38 @@
 import React from "react";
-// import GoogleMapReact from "google-map-react";
-// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-// import {LocationOutlinedIcon } from "@material-ui/icons/LocalActivityOutlined";
-// import { Rating } from "@material-ui/lab";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import {LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
 
-// import makeStyles from "./style";
+import makeStyles from "./style";
 
-const Map = () => {
-    // const classes = makeStyles();
-    // const isMoblie = useMediaQuery('(min-with:600px)');
+const Map = ({setCoordinates, setBounds, coordinates }) => {
+    const classes = makeStyles();
+    const isMoblie = useMediaQuery('(min-with:600px)');
 
-    // const coodinates = { lat: 0, lng: 0};
+ 
     return (
-        // <div className={classes.mapContainer}>
-        //     <GoogleMapReact 
-        //         bootstrapURLKeys={{key: ''}}
-        //         defaultCenter={coodinates}
-        //         center={coodinates}
-        //         defaultZoom={14}
-        //         margin={[50,50,50,50]}
-        //         options={}
-        //         onChange={}
-        //     >
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key: 'AIzaSyDWC6zeN1h1Z4jrdKPSq0EgITMFhS5F_rM'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={(e)=>{
+                    console.log(e);
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng});
+                }}
+                onChildClick={''}
+            
+            >
 
-        //     </GoogleMapReact>
+            </GoogleMapReact>
 
-        // </div>
+        </div>
 
-        <h1>Map</h1>
+        // <h1>Map</h1>
     );
 }
 
